976a9379cc3216f46146272f82bf1be16c357120
janet68 <46169449+janet6868@users.noreply.github.com>
2024-08-25 23:11:07 +0000

    updating all files

--- agenda.js
+++ agenda.js
@@ -9,14 +9,14 @@ document.addEventListener('DOMContentLoaded', function() {
        <table border="1">
   <tr><th>Time</th><th>Task</th><th>Task Manager</th><th>Moderator</th><th>Resources</th></tr>
   <tr><td>0700 - 0830</td><td>Registration</td><td>E. Adjowa, A. Bah</td><td></td><td>Name Tags, registration/sign-in sheets</td></tr>
-  <tr><td>0830 - 0840</td><td>Agenda and metting objectives</td><td>M. Sayo</td><td>M Garba</td><td>Projector, Screen, Sound system</td></tr>
+  <tr><td>0830 - 0840</td><td>Agenda and meeting objectives</td><td>K Gelaye</td><td>M Garba</td><td>Projector, Screen, Sound system</td></tr>
   <tr><td rowspan="2">0840 - 0915</td><td>Welcome and Remarks</td><td>P Bartel & M Bako</td><td></td><td rowspan="2">Projector, Screen, Sound system</td></tr>
   <tr><td>Each speaker has 5 minutes: NASA Rep & USAID Rep</td><td>NASA Rep & USAID Rep</td><td></td></tr>
   <tr><td>0915 - 0930</td><td>Participant Introductions (Each participant introduces the person to the right. Name, Affiliation)</td><td>All</td><td>M Garba</td><td>Microphone</td></tr>
   <tr><td>0930 - 1030</td><td>Successes; video and stories</td><td>T Niang</td><td>M Bako</td><td>Projector, Screen, Sound system</td></tr>
   <tr><td>1030 - 1100</td><td colspan="4">Coffee Break</td></tr>
   <tr><td>1100 - 1200</td><td>Deliverables, ARLs & Impacts (PMP) with discussion</td> <td>PCS Traore</td> <td>PCS Traore</td><td>Projector, Screen, Sound system</td>
-  <tr><td>1200 - 1300</td><td>Challenges Identified and Steps Taken</td> <td>M. Sayo</td> <td>PCS Traore</td><td>Projector, Screen, Sound system</td> 
+  <tr><td>1200 - 1300</td><td>Challenges Identified and Steps Taken</td> <td>M Sayo</td> <td>PCS Traore</td><td>Projector, Screen, Sound system</td> 
   </tr><tr><td>1300 - 1400</td><td colspan="4">Lunch</td></tr>
   <tr><td rowspan="5">1400 - 1530</td><td>Pause & Reflect(5 Breakout 5 groups)</td><td>M Sayo</td><td>T Niang</td><td rowspan="5">Flip Charts, Pens, Tables</td></tr>
   <tr><td>What went well last year?</td><td>J Mutuku</td><td></td></tr>
